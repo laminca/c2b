aafed74337d27bdbe95c0bf30e31fbed17bdee23
laminca <108716845+laminca@users.noreply.github.com>
2022-09-15 14:13:02 +0300

    Update confirmation_url.php

--- confirmation_url.php
+++ confirmation_url.php
@@ -4,11 +4,30 @@ header("Content-Type: application/json");
 
 $response = '{
     "ResultCode": 0,
-    "ResultDesc":"Confirmstion Received Successfully"
+    "ResultDesc":"Confirmation Received Successfully"
 
 }';
 //Data
 $mpesaResponse = file_get_contents('php://input');
+$url='https://webhook.site/ca8f670e-11e1-4b1a-9148-59f7e2518765/';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+//curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+
+
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+curl_setopt($ch,CURLOPT_POST,true);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$mpesaResponse);
+$response= curl_exec($ch);
+
+if($e =curl_error($ch)){
+
+    echo $e;
+}
+
+
 
 $logfile="mpesa_response.txt";
 
@@ -18,5 +37,4 @@ $log = fopen($logfile, "a");
 fwrite($log, $mpesaResponse);
 fclose ($log);
 echo $response;
-file_put_contents($logfile,file_get_contents('php://input'));
 ?>
