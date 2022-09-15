7ac75c08a0cd01149c64ad989161fb268d11294c
laminca <108716845+laminca@users.noreply.github.com>
2022-09-15 17:18:27 +0300

    Update confirmation_url.php

--- confirmation_url.php
+++ confirmation_url.php
@@ -10,19 +10,12 @@ $response = '{
 //Data
 $mpesaResponse = file_get_contents('php://input');
 
-if($e =curl_error($ch)){
 
-    echo $e;
-}
+$logfile=dirname(__FILE__).'/laminca-c2b.herokuapp.com/my_response.txt';
 
+$log = fopen($logfile, "a+");
 
-
-$logfile="/laminca-c2b.herokuapp.com/my_response.txt";
-
-$json = json_decode($mpesaResponse,true);
-
-$log = fopen($logfile, "a");
 fwrite($log, $mpesaResponse);
 fclose ($log);
-echo $response;
+echo $mpesaResponse;
 ?>
