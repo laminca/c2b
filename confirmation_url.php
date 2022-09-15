d75c751ccaa4f4f58f43e7fdd730b617e4bff111
laminca <108716845+laminca@users.noreply.github.com>
2022-09-15 14:20:49 +0300

    Update confirmation_url.php

--- confirmation_url.php
+++ confirmation_url.php
@@ -9,18 +9,6 @@ $response = '{
 }';
 //Data
 $mpesaResponse = file_get_contents('php://input');
-$url='https://webhook.site/ca8f670e-11e1-4b1a-9148-59f7e2518765/';
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-//curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
-
-
-
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
-curl_setopt($ch,CURLOPT_POST,true);
-curl_setopt($ch, CURLOPT_POSTFIELDS,$mpesaResponse);
-$response= curl_exec($ch);
 
 if($e =curl_error($ch)){
 
@@ -33,7 +21,7 @@ $logfile="mpesa_response.txt";
 
 $json = json_decode($mpesaResponse,true);
 
-$log = fopen($logfile, "a");
+$log = fopen("mpesa_response.txt", "a");
 fwrite($log, $mpesaResponse);
 fclose ($log);
 echo $response;
