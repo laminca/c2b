1a4a9cc275f3533f181af4442e9ac307d6d4fb9a
laminca <108716845+laminca@users.noreply.github.com>
2022-09-15 14:16:19 +0300

    Update register_url.php

--- register_url.php
+++ register_url.php
@@ -1,6 +1,6 @@
 
 <?php
-/*
+
 $url='https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
 $ch = curl_init();
 curl_setopt($ch , CURLOPT_URL, $url);
@@ -27,28 +27,27 @@ else
 
 }
 curl_close($ch);
-*/
+
 
 
 $url='https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
-//$headers =['Authorization: Bearer '.$access_token,'Content-Type: application/json'];
+$headers =['Authorization: Bearer '.$access_token,'Content-Type: application/json'];
 //$headers =['Content-Type: application/json','Authorization: Bearer '.$access_token];
 
 
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Authorization: Bearer wxEMGG9npX3A8QcbJRYQqVWST8qn',
-    'Content-Type: application/json'
-]);
 
-//curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_POST, 1);
 $confirmation_url='https://laminca-c2b.herokuapp.com/confirmation_url.php';
 $validation_url='https://laminca-c2b.herokuapp.com/validation.php';
 //"ShortCode"=>600426
+//$confirmation_url='http://localhost/Daraja%20ofl/c2b/confirmation_url.php';
+//$validation_url='http://localhost/Daraja%20ofl/c2b/validation.php';
 $reg_data=json_encode(array(
-    "ShortCode"=>600426,
+    "ShortCode"=>600984,
     "ResponseType"=>"Confirmed",
     "ConfirmationURL"=>$confirmation_url,
     "ValidationURL"=>$validation_url
@@ -60,12 +59,27 @@ $response= curl_exec($ch);
 curl_close($ch);
 echo $response;
 
+/*
+
+
+$ch = curl_init('https://sandbox.safaricom.co.ke/mpesa/c2b/v1/simulate');
+curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+curl_setopt($ch, CURLOPT_POST, 1);
+$data = json_encode(array(
+    "ShortCode"=> 600426,
+    "CommandID"=>"CustomerBuyGoodsOnline",
+    "Amount"=>"100",
+    "Msisdn"=>"254112605422",
+    "BillRefNumber"=>"",
+  ));
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response     = curl_exec($ch);
+curl_close($ch);
+echo $response;
 
 
 
-
-
-
-
+*/
 
 ?>
